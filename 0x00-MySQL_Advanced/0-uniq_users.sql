b0f6ef63c91525287255496d0e2eb577d9f0c212
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-01-18 17:50:50 +0000

    add task 0 to 2

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- create a table users
+CREATE TABLE IF NOT EXISTS users (
+	id INTEGER NOT NULL AUTO_INCREMENT,
+       	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY (id));
