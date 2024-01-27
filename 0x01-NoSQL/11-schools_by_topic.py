0090f81c5181b4b114d530664f94c181626cdc82
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-01-26 18:30:51 -0800

    add 11, 12 task

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""this script to find doc in a collection"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """find a doc into collection"""
+    return mongo_collection.find({'topics': {'$in': [topic]}})
--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" 11-main """
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    print(f'{logs.count_documents({})} logs')
+    m = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in m:
+        count = logs.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+    print(f'{logs.count_documents({"path":"/status"})} status check')
